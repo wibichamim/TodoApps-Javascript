66db793417fafe470b65f0dd0ac2f0016c874ff8
wibichamim <wibi.mushodiq@students.amikom.ac.id>
2020-09-30 19:25:06 +0700

    Add Filter
    Filter for displaying completed or uncompleted task

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".btn-add");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 //Event Listener
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteItem);
+filterOption.addEventListener("click", filterTodo);
 
 //Functions
 function addTodo(event) {
@@ -49,4 +51,29 @@ function deleteItem(e) {
     const todo = item.parentElement;
     todo.classList.toggle("completed");
   }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
 }
